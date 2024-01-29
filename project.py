f6849c7f525c54a28c5e8a16519950498ec270f0
Zahid Toruk <150917933+ZahidBahadirToruk@users.noreply.github.com>
2024-01-30 00:39:36 +0300

    the score cant go below 0 and i stocked the questions in a dictionary

--- project.py
+++ project.py
@@ -24,9 +24,9 @@ if __name__ == "__main__":
     quiz.ask_question('Who wrote the play "Romeo and Juliet"?', 'William Shakespeare')
     quiz.ask_question('What is the largest ocean on Earth?', 'Pacific')
 
-    if quiz.point > 3:
+    if quiz.point > 0:
         print('Congratulations! You scored', quiz.point, 'points. Well done!')
-    elif quiz.point < 0:
+    elif quiz.point <= 0:
         print('Try again. Your score is', quiz.point, '. Better luck next time!')
     else:
         print('Your final score is', quiz.point, '. Good effort!')
